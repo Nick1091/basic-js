659a983f5bf4075166a21e8b93af2b0e8954cd46
Nick1091 <nick10.91@mail.ru>
2021-09-21 01:50:19 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  if (options.addition === null) {
+    options.addition = "null";
+  }
+  if (!options.additionRepeatTimes){
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+  if(!options.additionSeparator){
+    options.additionSeparator = '|';
+  }
+  if(!options.separator){
+    options.separator = '+';
+  }
+  let newAddition = Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+  let result = Array(options.repeatTimes).fill(str + newAddition).join(options.separator);
+  return result;
 }
